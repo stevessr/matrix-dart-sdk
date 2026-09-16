79f76f6e2ca5c587afc072ef4f0b10c49eb4d6c6
stevessr <2984716340@qq.com>
2026-09-16 20:48:01 +0800

    test(markdown): simplify LaTeX expectations

--- test/markdown_latex_complex_test.dart
+++ test/markdown_latex_complex_test.dart
@@ -10,8 +10,7 @@ void main() {
     test('keeps escaped dollars inside one math node', () {
       expect(
         markdown(r'cost $\text{price \$5}$'),
-        r'cost <span data-mx-maths="\text{price \$5}"><code>\text{price \$5}</code></span>'
-            .replaceAll(r'\"', '"'),
+        r'cost <span data-mx-maths="\text{price \$5}"><code>\text{price \$5}</code></span>',
       );
     });
 
