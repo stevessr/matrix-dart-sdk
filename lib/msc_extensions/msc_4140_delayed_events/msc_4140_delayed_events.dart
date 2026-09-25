1721b10c34e8f6e5e735a188c7ac3cfd4693a0cf
stevessr <2984716340@qq.com>
2026-09-25 19:11:14 +0800

    fix(msc4140): implement accepted delayed-event API

--- lib/msc_extensions/msc_4140_delayed_events/msc_4140_delayed_events.dart
+++ lib/msc_extensions/msc_4140_delayed_events/msc_4140_delayed_events.dart
@@ -11,132 +11,123 @@ import '../../matrix.dart';
 enum DelayedEventAction { send, cancel, restart }
 
 extension DelayedEventsHandler on Client {
-  static const _delayedEventsEndpoint =
-      '_matrix/client/unstable/org.matrix.msc4140/delayed_events';
+  static const _unstableFeature = 'org.matrix.msc4140';
+  static const _stableFeature = 'org.matrix.msc4140.stable';
 
-  /// State events can be sent using this endpoint.  These events will be
-  /// overwritten if `<room id>`, `<event type>` and `<state key>` all
-  /// match.
+  Future<bool> isMsc4140Supported() async {
+    final versions = await getVersions();
+    return versions.unstableFeatures?[_stableFeature] == true ||
+        versions.unstableFeatures?[_unstableFeature] == true;
+  }
+
+  Future<bool> isMsc4140StableSupported() async {
+    final versions = await getVersions();
+    return versions.unstableFeatures?[_stableFeature] == true;
+  }
+
+  /// Schedules a delayed message or state event as defined by MSC4140.
   ///
-  /// Requests to this endpoint **cannot use transaction IDs**
-  /// like other `PUT` paths because they cannot be differentiated from the
-  /// `state_key`. Furthermore, `POST` is unsupported on state paths.
-  ///
-  /// The body of the request should be the content object of the event; the
-  /// fields in this object will vary depending on the type of event. See
-  /// [Room Events](https://spec.matrix.org/unstable/client-server-api/#room-events) for the `m.` event specification.
-  ///
-  /// If the event type being sent is `m.room.canonical_alias` servers
-  /// SHOULD ensure that any new aliases being listed in the event are valid
-  /// per their grammar/syntax and that they point to the room ID where the
-  /// state event is to be sent. Servers do not validate aliases which are
-  /// being removed or are already present in the state event.
-  ///
-  ///
-  /// [roomId] The room to set the state in
-  ///
-  /// [eventType] The type of event to send.
-  ///
-  /// [stateKey] The state_key for the state to send. Defaults to the empty string. When
-  /// an empty string, the trailing slash on this endpoint is optional.
-  ///
-  /// [delayInMs] Optional number of milliseconds the homeserver should wait before sending the event.
-  /// If no delay is provided, the event is sent immediately as normal.
-  ///
-  /// [body]
-  ///
-  /// returns `event_id`:
-  /// A unique identifier for the event.
-  /// If a delay is provided, the homeserver schedules the event to be sent with the specified delay
-  /// and responds with an opaque delay_id field (omitting the event_id as it is not available)
-  Future<String> setRoomStateWithKeyWithDelay(
-    String roomId,
-    String eventType,
-    String stateKey,
-    int? delayInMs,
-    Map<String, Object?> body,
-  ) async {
+  /// Until MSC4140 lands in a released Matrix specification, homeservers
+  /// advertise `org.matrix.msc4140.stable` when the stable endpoint shape is
+  /// available. Otherwise the unstable endpoint is used when
+  /// `org.matrix.msc4140` is advertised.
+  Future<String> scheduleDelayedEvent({
+    required String roomId,
+    required String eventType,
+    required String txnId,
+    required int delayInMs,
+    required Map<String, Object?> content,
+    String? stateKey,
+  }) async {
+    if (delayInMs <= 0) {
+      throw ArgumentError.value(delayInMs, 'delayInMs', 'must be positive');
+    }
+
+    final versions = await getVersions();
+    final stable = versions.unstableFeatures?[_stableFeature] == true;
+    final unstable = versions.unstableFeatures?[_unstableFeature] == true;
+    if (!stable && !unstable) {
+      throw UnsupportedError('Homeserver does not advertise MSC4140 support');
+    }
+
+    final prefix = stable
+        ? '_matrix/client/v3'
+        : '_matrix/client/unstable/org.matrix.msc4140';
     final requestUri = Uri(
       path:
-          '_matrix/client/v3/rooms/${Uri.encodeComponent(roomId)}/state/${Uri.encodeComponent(eventType)}/${Uri.encodeComponent(stateKey)}',
-      queryParameters: {
-        if (delayInMs != null) 'org.matrix.msc4140.delay': delayInMs.toString(),
-      },
+          '$prefix/rooms/${Uri.encodeComponent(roomId)}/delayed_event/${Uri.encodeComponent(eventType)}/${Uri.encodeComponent(txnId)}',
     );
 
     final request = http.Request('PUT', baseUri!.resolveUri(requestUri));
     request.headers['authorization'] = 'Bearer ${bearerToken!}';
     request.headers['content-type'] = 'application/json';
-    request.bodyBytes = utf8.encode(jsonEncode(body));
+    request.bodyBytes = utf8.encode(
+      jsonEncode({
+        'delay_ms': delayInMs,
+        if (stateKey != null) 'state_key': stateKey,
+        'content': content,
+      }),
+    );
     final response = await httpClient.send(request);
     final responseBody = await response.stream.toBytes();
     if (response.statusCode != 200) unexpectedResponse(response, responseBody);
-    final responseString = utf8.decode(responseBody);
-    final json = jsonDecode(responseString);
-    return json['event_id'] ?? json['delay_id'] as String;
+
+    final json = jsonDecode(utf8.decode(responseBody)) as Map<String, dynamic>;
+    return json['delay_id'] as String;
   }
 
+  /// Applies [action] to a scheduled delayed event.
   Future<void> manageDelayedEvent(
-    String delayedId,
-    DelayedEventAction delayedEventAction,
+    String delayId,
+    DelayedEventAction action,
   ) async {
-    final requestUri = Uri(path: '$_delayedEventsEndpoint/$delayedId');
+    final versions = await getVersions();
+    final stable = versions.unstableFeatures?[_stableFeature] == true;
+    final unstable = versions.unstableFeatures?[_unstableFeature] == true;
+    if (!stable && !unstable) {
+      throw UnsupportedError('Homeserver does not advertise MSC4140 support');
+    }
+
+    final prefix = stable
+        ? '_matrix/client/v1/delayed_events'
+        : '_matrix/client/unstable/org.matrix.msc4140/delayed_events';
+    final requestUri = Uri(
+      path:
+          '$prefix/${Uri.encodeComponent(delayId)}/${Uri.encodeComponent(action.name)}',
+    );
 
     final request = http.Request('POST', baseUri!.resolveUri(requestUri));
     request.headers['authorization'] = 'Bearer ${bearerToken!}';
     request.headers['content-type'] = 'application/json';
-    request.bodyBytes = utf8.encode(
-      jsonEncode({'action': delayedEventAction.name}),
-    );
+    request.bodyBytes = utf8.encode(jsonEncode(<String, Object?>{}));
     final response = await httpClient.send(request);
     final responseBody = await response.stream.toBytes();
     if (response.statusCode != 200) unexpectedResponse(response, responseBody);
   }
 
-  // This should use the /delayed_events/scheduled endpoint
-  // but synapse implementation uses the /delayed_events
-  Future<ScheduledDelayedEventsResponse> getScheduledDelayedEvents({
-    String? from,
-  }) async {
-    final requestUri = Uri(
-      path: _delayedEventsEndpoint,
-      queryParameters: {'from': ?from},
-    );
-
-    final request = http.Request('GET', baseUri!.resolveUri(requestUri));
-    request.headers['authorization'] = 'Bearer ${bearerToken!}';
-    request.headers['content-type'] = 'application/json';
-    final response = await httpClient.send(request);
-    final responseBody = await response.stream.toBytes();
-    if (response.statusCode != 200) {
-      return await _getScheduledDelayedEventsAccordingToSpec(from: from);
+  /// Retrieves one scheduled or recently finalised delayed event.
+  Future<DelayedEvent> getDelayedEvent(String delayId) async {
+    final versions = await getVersions();
+    final stable = versions.unstableFeatures?[_stableFeature] == true;
+    final unstable = versions.unstableFeatures?[_unstableFeature] == true;
+    if (!stable && !unstable) {
+      throw UnsupportedError('Homeserver does not advertise MSC4140 support');
     }
-    final responseString = utf8.decode(responseBody);
-    final json = jsonDecode(responseString);
-    final res = ScheduledDelayedEventsResponse.fromJson(json);
-    return res;
-  }
 
-  // maybe the synapse impl changes, I don't want stuff to break
-  Future<ScheduledDelayedEventsResponse>
-  _getScheduledDelayedEventsAccordingToSpec({String? from}) async {
+    final prefix = stable
+        ? '_matrix/client/v1/delayed_events'
+        : '_matrix/client/unstable/org.matrix.msc4140/delayed_events';
     final requestUri = Uri(
-      path: '$_delayedEventsEndpoint/scheduled',
-      queryParameters: {'from': ?from},
+      path: '$prefix/${Uri.encodeComponent(delayId)}',
     );
 
     final request = http.Request('GET', baseUri!.resolveUri(requestUri));
     request.headers['authorization'] = 'Bearer ${bearerToken!}';
-    request.headers['content-type'] = 'application/json';
     final response = await httpClient.send(request);
     final responseBody = await response.stream.toBytes();
     if (response.statusCode != 200) unexpectedResponse(response, responseBody);
-    final responseString = utf8.decode(responseBody);
-    final json = jsonDecode(responseString);
-    final res = ScheduledDelayedEventsResponse.fromJson(json);
-    return res;
-  }
 
-  /// TODO: implement the remaining APIs
-  /// GET /_matrix/client/unstable/org.matrix.msc4140/delayed_events/finalised
+    final json = jsonDecode(utf8.decode(responseBody)) as Map<String, dynamic>;
+    return DelayedEvent.fromJson(json);
+  }
 }
